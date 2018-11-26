3b6f2b1c467571855c1a2b0ecfef5175f7c944a1
Hailey June <hailey.june.b@gmail.com>
2018-11-26 13:04:07 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [,] table = new int [10, 10];
+
+            for (var i=0; i<10; i++)
+            {
+                for (var j=0; j<10; j++){
+                    table[i,j] = (i+1)*(j+1);
+                }
+            }
+
+            for (var i=0; i<10; i++)
+            {
+                Console.Write("[");
+                for (var j=0; j<10; j++){
+                    Console.Write(table[i,j] + ", ");
+                }
+                Console.Write("]");
+                Console.WriteLine();
+            }
+        }
+    }
+}
